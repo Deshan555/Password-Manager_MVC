97f4782048574ba11b2ab352ec327cdb2d0ab733
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-08-06 23:20:39 +0530

    reconfigure and restructure configure file

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
 
 // database details 
-$database = 'password_manager';
-$server = 'localhost';
-$port = 3306;
-$username = 'root';
-$password = '';
+$db = [
+    'db_name' => 'password_manager',
+    'db_host' => 'localhost',
+    'db_port' => 3306,
+    'db_username' => 'root',
+    'db_password' => '',
+];
+
+foreach($db as $key => $value){
+    $GLOBALS[$key] = $value;
+}
