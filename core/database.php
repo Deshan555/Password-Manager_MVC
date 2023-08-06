fdfdd4550695ef1139fd32d7030177c97ccba589
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-08-06 23:33:02 +0530

    update database connection logic

--- core/database.php
+++ core/database.php
@@ -1,1 +1,50 @@
 <?php
+require 'config.php';
+
+/*try{
+    $conn = new PDO("mysql:host=$server;dbname=$database;dbport=$port", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Connected successfully";
+
+}catch(PDOException $e){
+    
+    die("Connection Faild : ".$e->getMessage());
+}*/
+
+class Database{
+    private $conn;
+    private $db_host;
+    private $db_name;
+    private $db_port;
+    private $db_username;
+    private $db_password;
+
+    public function __construct()
+    {
+        $this->db_host = $GLOBALS['db_host'];
+        $this->db_name = $GLOBALS['db_name'];
+        $this->db_port = $GLOBALS['db_port'];
+        $this->db_username = $GLOBALS['db_username'];
+        $this->db_password = $GLOBALS['db_password'];
+
+        try{
+            $options = [
+                PDO::ATTR_ERRMODE => true,
+                PDO::ERRMODE_EXCEPTION => PDO::ERRMODE_EXCEPTION
+            ];
+            $conn = new PDO("mysql:host=$this->db_host;dbname=$this->db_name;dbport=$this->db_port", $this->db_username, $this->db_password, $options);
+            $this->conn = $conn;
+            echo "Connected successfully";
+        }catch(PDOException $e){
+            
+            die("Connection Faild : ".$e->getMessage());
+        }
+    }
+
+    public function query($sql){
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
+    }
+}
