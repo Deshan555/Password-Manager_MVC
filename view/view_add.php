205c5e231d7759ee24a59d5610a4d363cf35bb73
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-08-09 14:56:42 +0530

    create view for add new passwords

---
+++ view/view_add.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+      <!-- Font Awesome -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+  <!-- MDB -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" />
+</head>
+<body class="container">
+<form action="/action_page.php">
+<div class="form-group">
+    <label for="pwd">Account Name:</label>
+    <input type="text" class="form-control" id="pwd">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Host Addess:</label>
+    <input type="link" class="form-control" id="pwd">
+  </div>
+  <div class="form-group">
+    <label for="email">Email address/User Name:</label>
+    <input type="text" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+</body>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"></script>
+</html>
