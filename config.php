25af3194a58437a14e47e6a9c8a8c4a551b655dc
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-08-09 19:31:03 +0530

    update configuration file

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ $db = [
     'db_port' => 3306,
     'db_username' => 'root',
     'db_password' => '',
+    'site_url' => 'http://localhost/PHP-Mvc/index.php',
 ];
 
 foreach($db as $key => $value){
